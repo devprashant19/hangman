27f24f7f762c900469533bb488b3e20f5d43d44a
aniketchauhan18 <workwithaniket18@gmail.com>
2025-09-04 14:36:27 +0000

    feat: add backend call logic, update: word-list

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,30 @@ const playAgainBtn = gameModal.querySelector("button");
 let currentWord, correctLetters, wrongGuessCount;
 const maxGuesses = 6;
 
+// score type : number
+async function sendScoreToBackend(score) {
+    try {
+        const urlParams = new URLSearchParams(window.location.search);
+        const userId = urlParams.get("userId");
+        console.log(userId);
+        // add backend url here
+        const res = await fetch("http://localhost:3000", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                score: score,
+                // TODO: add it
+            })
+        })
+        const result = await res.json();
+        console.log(result);
+    } catch {
+        console.log(err);
+    }
+}
+
 const resetGame = () => {
     // Ressetting game variables and UI elements
     correctLetters = [];
@@ -28,21 +52,34 @@ const getRandomWord = () => {
     resetGame();
 }
 
-const gameOver = (isVictory) => {
+const gameOver = async (isVictory) => {
     // After game complete.. showing modal with relevant details
     const modalText = isVictory ? `You found the word:` : 'The correct word was:';
     gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
     gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
+
     gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
     gameModal.classList.add("show");
+    const urlParams = new URLSearchParams(window.location.search);
+    let chances = parseInt(urlParams.get("chances") || "0", 10);
+    if (!isVictory) {
+        chances--;
+        urlParams.set("chances", chances);
+        window.history.replaceState(null, '', `${window.location.pathname}?${urlParams}`);
+    } else {
+        // TODO: add current score here
+        await sendScoreToBackend(10);
+    }
+
 }
 
+http://localhost:5500/?id=%22sss%22&chances=1
 const initGame = (button, clickedLetter) => {
     // Checking if clickedLetter is exist on the currentWord
-    if(currentWord.includes(clickedLetter)) {
+    if (currentWord.includes(clickedLetter)) {
         // Showing all correct letters on the word display
         [...currentWord].forEach((letter, index) => {
-            if(letter === clickedLetter) {
+            if (letter === clickedLetter) {
                 correctLetters.push(letter);
                 wordDisplay.querySelectorAll("li")[index].innerText = letter;
                 wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
@@ -57,8 +94,8 @@ const initGame = (button, clickedLetter) => {
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 
     // Calling gameOver function if any of these condition meets
-    if(wrongGuessCount === maxGuesses) return gameOver(false);
-    if(correctLetters.length === currentWord.length) return gameOver(true);
+    if (wrongGuessCount === maxGuesses) return gameOver(false);
+    if (correctLetters.length === currentWord.length) return gameOver(true);
 }
 
 // Creating keyboard buttons and adding event listeners
